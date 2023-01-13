3a1a4005cdfd907467a964e005d53f28d31c6098
Akhmed Al-Sayed <ahmed071998@gmail.com>
2023-01-13 20:12:21 +0100

    Fixed 'tree' funciton in git_tree.sh script
    The issue was in funciton variables not being set as 'local',
    which caused them to be passed ovr in recursive calls, resulting
    in weird behaviour.

--- git_tree.sh
+++ git_tree.sh
@@ -3,11 +3,40 @@
 # Script that pritns out a tree of files in a git-repo along with simple stats
 # on changes done in each file.
 
+
+
+tree_dfs () {
+  local curr_dir="$1"
+  local indent="$2"
+  
+  # echo $curr_dir
+  
+  local children=(`ls $curr_dir`)
+
+  local last=$((${#children[@]}-1))
+
+  for i in ${!children[@]}; do
+    if [[ $i -ne $last ]]; then
+      echo "${indent}├── ${children[$i]}"
+    else
+      echo "${indent}└── ${children[$i]}"
+    fi
+    
+    if [[ -d "${curr_dir}/${children[$i]}" ]]; then
+      if [[ $i -ne $last ]]; then
+        tree_dfs "${curr_dir}/${children[$i]}" "$indent│   "
+      else
+        tree_dfs "${curr_dir}/${children[$i]}" "$indent    "
+      fi
+    fi
+  done
+}
+
+
 # Colors for output
 GREEN='\e[32m'
 RED='\e[31m'
 END='\e[0m'
-
 
 # Default depth history search
 DEPTH=5
@@ -28,17 +57,18 @@ commit_id_list=`git log | grep -E "commit\ [0-9a-f]{40}" | grep -oEw "[0-9a-f]{4
 
 
 for commit_id in $commit_id_list; do
-  commit_info=`git show $commit_id --numstat --oneline | grep -Eo "[0-9]*\s[0-9]*\s[a-zA-Z0-9]*\.*[a-zA-Z0-9]+$"`
+  commit_info=`git show $commit_id --numstat --oneline | grep -Eo "[0-9]+\s[0-9]+\s[a-zA-Z0-9_\.\$]+$"`
+  # echo ">${commit_info}"
   
   list=($commit_info)
   
   added=${list[0]}
   subbed=${list[1]}
   fname=${list[2]}
-  # printf "${fname} [${GREEN}${added}${END}/${RED}${subbed}${END}]"
-  echo -e $RED $commit_info $END
-  echo ""
+  # echo -e "${fname} [${GREEN}${added}${END}/${RED}${subbed}${END}]"
+  # echo -e $RED $commit_info $END
+  # echo ""
   # echo $commit_id
 done
 
-
+tree_dfs "/home/akhmed" ""
