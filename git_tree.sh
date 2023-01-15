4d2d230def9e4bdf13268daa76c2a44e686e452d
Akhmed Al-Sayed <ahmed071998@gmail.com>
2023-01-15 23:29:52 +0100

    Finished working protoype of 'git tree' script
    Now git_tree.sh outputs a tree with code changes. A postfix is now
    being added to files with any changes.
    
    To improve:
    x Loops can be optimized to run in linear time
    x Options for desired depth and extra info can be added

--- git_tree.sh
+++ git_tree.sh
@@ -2,31 +2,43 @@
 # 
 # Script that pritns out a tree of files in a git-repo along with simple stats
 # on changes done in each file.
+#
+# Author: Akhmed Al-Sayed
+# Last update: 15/1/2023
 
-
-
-tree_dfs () {
+tree () {
   local curr_dir="$1"
   local indent="$2"
   
   # echo $curr_dir
   
   local children=(`ls $curr_dir`)
-
   local last=$((${#children[@]}-1))
 
+  local postfix=""
+
   for i in ${!children[@]}; do
+    # Adding postfix to changed files
+    if [[ "${files_changed[@]}" =~ (^|[[:space:]])${children[$i]}($|[[:space:]]) ]]; then
+      for j in "${!files_changed[@]}"; do
+        if [[ "${files_changed[$j]}" = "${children[$i]}" ]]; then
+          postfix="[${GREEN}${greens_list[$j]}↑${END} ${RED}${reds_list[$j]}↓${END}]"
+        fi
+      done
+    fi
+
+
     if [[ $i -ne $last ]]; then
-      echo "${indent}├── ${children[$i]}"
+      echo -e "${indent}├── ${children[$i]} ${postfix}"
     else
-      echo "${indent}└── ${children[$i]}"
+      echo -e "${indent}└── ${children[$i]} ${postfix}"
     fi
     
     if [[ -d "${curr_dir}/${children[$i]}" ]]; then
       if [[ $i -ne $last ]]; then
-        tree_dfs "${curr_dir}/${children[$i]}" "$indent│   "
+        tree "${curr_dir}/${children[$i]}" "$indent│   "
       else
-        tree_dfs "${curr_dir}/${children[$i]}" "$indent    "
+        tree "${curr_dir}/${children[$i]}" "$indent    "
       fi
     fi
   done
@@ -38,37 +50,49 @@ GREEN='\e[32m'
 RED='\e[31m'
 END='\e[0m'
 
+
 # Default depth history search
 DEPTH=5
 
 
-
 files_changed=()
-# count_added=()
-# count_removed=()
-
-is_new=()
+greens_list=()
+reds_list=()
 
 
-commit_id_list=`git log | grep -E "commit\ [0-9a-f]{40}" | grep -oEw "[0-9a-f]{40}" --color=never`
-
-
-# echo $commit_id_list
+commit_id_list=`git log -n ${DEPTH} | grep -E "commit\ [0-9a-f]{40}" | grep -oEw "[0-9a-f]{40}" --color=never`
 
 
 for commit_id in $commit_id_list; do
   commit_info=`git show $commit_id --numstat --oneline | grep -Eo "[0-9]+\s[0-9]+\s[a-zA-Z0-9_\.\$]+$"`
   # echo ">${commit_info}"
-  
+
   list=($commit_info)
-  
-  added=${list[0]}
-  subbed=${list[1]}
-  fname=${list[2]}
-  # echo -e "${fname} [${GREEN}${added}${END}/${RED}${subbed}${END}]"
+
+  greens=${list[0]}
+  reds=${list[1]}
+  file_name="${list[2]}"
+
+
+  if [[ "${files_changed[@]}" =~ (^|[[:space:]])${file_name}($|[[:space:]]) ]]; then
+    for i in "${!files_changed[@]}"; do
+      if [[ "${files_changed[$i]}" = "${file_name}" ]]; then
+        greens_list[$i]=$((${greens_list[$i]} + $greens))
+        reds_list[$i]=$((${reds_list[$i]} + $reds))
+      fi
+    done
+    # echo "not new"
+  else
+    files_changed+=("${file_name}")
+    greens_list+=($greens)
+    reds_list+=($reds)
+    # echo "new"
+  fi
+  # echo "List: ${files_changed[@]}" 
+  # echo -e "${file_name} [${GREEN}${greens}${END}/${RED}${reds}${END}]"
   # echo -e $RED $commit_info $END
   # echo ""
   # echo $commit_id
 done
 
-tree_dfs "/home/akhmed" ""
+tree "/srv/git/bdashing" ""
